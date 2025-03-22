3c84bf22e85eb040b003655c245ddd92249740f5
Prinu Dickson <prinu.dickson@pwc.com>
2025-03-22 20:49:51 +0100

    create custom User model for authentication. TDD. Works needs to be verified.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,53 @@
-from django.db import models # noqa
+"""
+Database Models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+    Manager for users
+
+    Parameters
+    ----------
+    BaseUserManager : _type_
+        _description_
+    """
+    def create_user(self, email, password=None, **extra_field):
+        """
+        Create, Save and return a new user
+
+        Parameters
+        ----------
+        email : _type_
+            _description_
+        password : _type_, optional
+            _description_, by default None
+        """
+        user = self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    User in the System
+
+    Parameters
+    ----------
+    AbstractBaseUser : _type_
+        _description_
+    PermissionsMixin : _type_
+        _description_
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
