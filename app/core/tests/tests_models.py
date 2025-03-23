e77e18ea7f8c37582da9a46cd0b841fd0bf4324f
prinudickson <prinudickson@gmail.com>
2025-03-23 17:46:31 +0100

    added the user models and logged in to admin portal first time, linting yet to be fixed.

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -17,7 +17,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_succesful(self):
         """
-        test creation of user with email succesful
+        Test creation of user with email succesful
         """
         email = 'test@example.com'
         password = 'test1234'
@@ -28,3 +28,36 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalised(self):
+        """
+        Test email is normalised for new users
+        """
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """
+        Test creating a user without email raises valueError
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test1234')
+
+    def test_create_superuser(self):
+        """
+        Test creating a superuser
+        """
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test1234'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
